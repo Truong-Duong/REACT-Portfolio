135cbf1ae6d67f3420a89174306d67c7551cd7a5
Truong Duong <truong.duong1908@gmail.com>
2021-10-12 13:57:37 -0500

    add a footers

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer
+			className="text-center text-white"
+			style={{ backgroundColor: '#f1f1f1' }}
+		>
+			<div
+				className="text-center text-dark p-3"
+				style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+			>
+				Designed and Created by Truong Duong | Copyright &copy;2021 Truong Duong. Confidential and Proprietary. All Rights Reserved.
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
